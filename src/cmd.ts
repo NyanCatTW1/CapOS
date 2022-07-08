99c8cb7cd6874b2da35692c66b9dec2bea6b21d8
Nyan Cat <17372086+e666666@users.noreply.github.com>
2022-07-08 19:01:29 +0800

    Add help command

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,6 +1,35 @@
 import {term, lineInput} from './term';
 import {printUsername, c} from './misc/termHelper';
 import {player} from './playerData';
+import {setupHelpCommand} from './commands/help';
+
+// I really hope there is a way to make this DRYer...
+// https://github.com/Microsoft/TypeScript/issues/5326
+export class Command {
+  name: string;
+  desc: string;
+  cmdHandler: (argc: number, argv: string[]) => number;
+
+  constructor({
+    name,
+    desc,
+    cmdHandler,
+  }: {
+    name: string;
+    desc: string;
+    cmdHandler: (argc: number, argv: string[]) => number;
+  }) {
+    this.name = name;
+    this.desc = desc;
+    this.cmdHandler = cmdHandler;
+  }
+}
+
+export const cmds: {[key: string]: Command} = {};
+
+export function addCommand(cmd: Command) {
+  cmds[cmd.name] = cmd;
+}
 
 function getPS() {
   return (
@@ -13,16 +42,22 @@ function getPS() {
 }
 
 export async function runCommandHandler() {
+  setupHelpCommand();
+
   for (;;) {
     const cmd = await lineInput(getPS());
-    const cmdName = cmd.split(' ')[0];
-    switch (cmdName) {
-      case '':
-        // No command fed
-        break;
-      default:
-        term.writeln('csh: command not found: ' + cmdName);
-        break;
+    const argv = cmd.split(' ');
+    const argc = argv.length;
+    const cmdName = argv[0];
+    if (cmdName === '') {
+      // No command fed
+      continue;
+    }
+
+    if (cmds[cmdName] instanceof Command) {
+      cmds[cmdName].cmdHandler(argc, argv);
+    } else {
+      term.writeln('csh: command not found: ' + cmdName);
     }
   }
 }
---
+++ src/commands/help.ts
@@ -0,0 +1,19 @@
+import {addCommand, Command, cmds} from '../cmd';
+import {term} from '../term'
+
+export function setupHelpCommand() {
+  addCommand(
+    new Command({
+      name: 'help',
+      desc: 'Lists all commands and their purposes',
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      cmdHandler: (_argc: number, _argv: string[]) => {
+        Object.keys(cmds).forEach((cmdName) => {
+          term.writeln(`${cmdName}: ${cmds[cmdName].desc}`);
+        });
+
+        return 0;
+      },
+    })
+  );
+}
